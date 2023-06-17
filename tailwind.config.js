e515d1fcf8bccf368b14ec1db71215cecc0a4d8d
AraZaynu <aradanish029@gmail.com>
2023-06-17 14:52:29 +0700

    Kertas-Gunting-Batu

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index2.html'],
+  theme: {
+    container: {
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors: {
+        'premier': '#181C25',
+        'graan':'#2DCAA7',
+        'graan2': '#2BAA8E',
+        'yel' : '#FFDE59',
+        'redd'  : '#FF3131',
+      },
+      fontFamily: {
+        IBM : "'IBM Plex Mono', monospace",
+        IBMS : "'IBM Plex Sans', sans-serif",
+        BGK : "'Press Start 2P', cursive;"
+      },
+    },
+  },
+  plugins: [],
+}
+
